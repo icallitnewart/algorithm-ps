3d1b35e6d198fb143026479580aaf69003922b52
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-03-20 11:38:16 +0900

    [Bronze III] Title: 공 넣기, Time: 188 ms, Memory: 10100 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.js
+++ 백준/Bronze/10810. 공 넣기/공 넣기.js
@@ -1,29 +1,17 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(n => n.split(' '));
-const [N, M] = input[0].map(n => Number(n));
+const [[N, M], ...rules] = input;
+let balls = [];
 
-//바구니 셋팅
-let basketObj = {};
-for (let n = 1; n <= N; n++) basketObj[n] = [];
+rules.forEach((rule) => {
+  const [i, j, k] = rule.map(n => Number(n));
+  for (let n = 1; n <= N; n++) {
+    const basketNum = balls[n - 1];
+    const isEmpty = (basketNum === undefined);
+    const inRange = (n >= i && n <= j);
 
-for (let a = 1; a <= M; a++) {
-  const rules = input[a];
-  //i번 바구니부터 j번 바구니까지에 k번 번호
-  const [i, j, k] = rules.map(n => Number(n));
-
-  for (let b = i; b <= j; b++) {
-    const basket = basketObj[b];
-    if (basket.includes(k)) {
-      const index = basket.indexOf(k);
-      basket.splice(index, 1);
-    }
-    basketObj[b].push(k);
+    if (isEmpty) balls.push(inRange ? k : 0);
+    else balls[n - 1] = (inRange) ? k : balls[n - 1];
   }
-}
+});
 
-let answer = [];
-for (const key in basketObj) {
-  const balls = Object.values(basketObj[key]);
-  const lastNumber = balls.length > 0 ? balls[balls.length - 1] : 0
-  answer.push(lastNumber);
-}
-console.log(answer.join(' '));
+console.log(balls.join(' '));
