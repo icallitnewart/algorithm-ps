86320dcfcaf313cd8f523bca28f1309e5bf2f8f5
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-03-20 11:57:06 +0900

    [Bronze III] Title: 공 넣기, Time: 164 ms, Memory: 9376 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.js
+++ 백준/Bronze/10810. 공 넣기/공 넣기.js
@@ -1,16 +1,12 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(n => n.split(' '));
 const [[N, M], ...rules] = input;
-let balls = [];
+let balls = new Array(Number(N)).fill(0);
 
 rules.forEach((rule) => {
   const [i, j, k] = rule.map(n => Number(n));
   for (let n = 1; n <= N; n++) {
-    const basketNum = balls[n - 1];
-    const isEmpty = (basketNum === undefined);
     const inRange = (n >= i && n <= j);
-
-    if (isEmpty) balls.push(inRange ? k : 0);
-    else balls[n - 1] = (inRange) ? k : balls[n - 1];
+    if (inRange) balls[n - 1] = k;
   }
 });
 
