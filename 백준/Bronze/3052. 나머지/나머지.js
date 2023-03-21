a5574039bbf25650bf9486a6e995a2d41e9f3a5e
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-03-21 09:44:44 +0900

    [Bronze II] Title: 나머지, Time: 120 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.js
+++ 백준/Bronze/3052. 나머지/나머지.js
@@ -1,5 +1,4 @@
 const numbers = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n').map(Number);
 const remainders = numbers.map(n => n % 42);
-const set = new Set(remainders);
-const uniqueNum = [...set];
-console.log(uniqueNum.length);
+const uniqueNum = new Set(remainders);
+console.log([...uniqueNum].length);
