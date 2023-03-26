ae93cdb088c6bdce3a3feba51e04c24ece8eeb04
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-03-26 22:08:56 +0900

    [Bronze II] Title: 단어의 개수, Time: 144 ms, Memory: 17900 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.js
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -1,6 +1,4 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
 
-console.log(
-  (input.length === 1 && !input[0])
-    ? 0 : input.length
-);
+if (input.length === 1 && !input[0]) console.log(0);
+else console.log(input.length);
