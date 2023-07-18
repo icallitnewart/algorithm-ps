4533e285931b0802046c3e699489ba72eee65842
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-07-18 11:41:59 +0900

    [level 0] Title: 특이한 정렬, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -0,0 +1,7 @@
+function solution(numlist, n) {
+    return numlist.sort((a, b)=> {
+        const A = Math.abs(a - n);
+        const B = Math.abs(b - n);
+        return (A === B) ? b - a : A - B;
+    });
+}
