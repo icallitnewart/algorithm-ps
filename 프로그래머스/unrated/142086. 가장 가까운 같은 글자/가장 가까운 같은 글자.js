d3d24be2f9d43fb0796086f140b32c04de037a56
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-07-20 12:01:39 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 42.57 ms, Memory: 37.2 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,8 +1,8 @@
 function solution(s) {
     const str = s.split('');
     return str.map((letter, i)=> {
-        const prevLetters = str.slice(0, i).reverse();
-        const index = prevLetters.indexOf(letter);
-        return index > -1 ? index + 1 : index;
+        const prevLetters = str.slice(0, i);
+        const index = prevLetters.lastIndexOf(letter);
+        return index > -1 ? prevLetters.length - index : -1;
     });
 }
