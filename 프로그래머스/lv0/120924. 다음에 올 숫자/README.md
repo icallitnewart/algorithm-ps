d9fcbb2c6b3c4dcaa1cdc01bf8b12abff6fafa5f
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-07-20 11:43:03 +0900

    [level 0] Title: 다음에 올 숫자, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120924. 다음에 올 숫자/README.md
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 0] 다음에 올 숫자 - 120924 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120924) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120924?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.08 ms
+메모리: 33.4 MB, 시간: 0.03 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
+++ 프로그래머스/lv0/120924. 다음에 올 숫자/다음에 올 숫자.js
@@ -1,7 +1,6 @@
 function solution(common) {
-    const [_, ...arith] = common.map((num, i)=> (i>0) ? num - common[i-1] : 0);
-    const isArithmetic = arith.every((num, i)=> num === arith[0]);
+    const isArithmetic = common[1] - common[0] === common[2] - common[1];
     return isArithmetic 
-            ? common[common.length - 1] + arith[0] 
+            ? common[common.length - 1] + (common[1] - common[0])
             : common[common.length - 1] * (common[1] / common[0]);
 }
