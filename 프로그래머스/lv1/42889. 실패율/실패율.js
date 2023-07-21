b343b3d9b0ee5c21fc0e8d18d030967a968437b8
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-07-21 12:55:25 +0900

    [level 1] Title: 실패율, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.js
+++ 프로그래머스/lv1/42889. 실패율/실패율.js
@@ -1,13 +1,13 @@
 function solution(N, stages) {
-    const rates = {};
+    let rates = [];
     for(let level=1; level<=N; level++) {
         const challenge = stages.filter(stage=> stage >= level).length;
         const success = stages.filter(stage=> stage === level).length; 
-        rates[level] = success / challenge;
+        rates = [...rates, { [level] : success / challenge }];
     }
     
-    return Object.entries(rates)
-                .map(([k, v])=> ({[k]: v}))
-                .sort((a, b)=> Object.values(b) - Object.values(a))
-                .map(obj=> Number(Object.keys(obj)[0]));
+    return rates
+            .sort((a, b)=> Object.values(b) - Object.values(a))
+            .map(obj=> Number(Object.keys(obj)[0]));
+    
 }
