9a762f12a7d868ac9d1a79325b38cbe57c493f4c
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-07-27 11:18:13 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.09 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.js
@@ -1,10 +1,4 @@
 function solution(n) {
-    let num = n + 1;
-    const getOnes = (v)=> v.toString(2).split('').filter(v=> v === '1').length;
-    while(num) {
-        if(getOnes(num) === getOnes(n)) break;
-        else num++;
-    }
-    
-    return num;
+    var answer = 0;
+    return answer;
 }
