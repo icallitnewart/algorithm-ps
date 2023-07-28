85293ba1777cb1d1cf7b8de5e511e5698c598fcf
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-07-28 11:56:32 +0900

    [level 2] Title: 전화번호 목록, Time: 107.14 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/lv2/42577. 전화번호 목록/README.md
+++ 프로그래머스/lv2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577#) 
 
 ### 성능 요약
 
-메모리: 75.4 MB, 시간: 99.36 ms
+메모리: 74 MB, 시간: 107.14 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.js
@@ -1,11 +1,4 @@
 function solution(phone_book) {
-    phone_book.sort();
-    
-    for(let i=0; i<phone_book.length; i++) {
-        if(phone_book[i].startsWith(phone_book[i-1])) {
-            return false;
-            break;
-        }
-    }
-    return true;
+    phone_book.sort();    
+    return !phone_book.some((n, i)=> n.startsWith(phone_book[i-1]));
 }
