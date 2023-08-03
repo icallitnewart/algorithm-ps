0d226ce3e855f37afa6f02293671de553b3caa07
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-08-03 11:39:47 +0900

    [level 2] Title: 영어 끝말잇기, Time: 3.93 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12981. 영어 끝말잇기/영어 끝말잇기.js
@@ -0,0 +1,19 @@
+function solution(n, words) {
+    const arr = [];
+    
+    for(let i=0; i<words.length; i++) {
+        const word = words[i];
+        const lastWord = arr[arr.length - 1];
+        
+        if((arr.length > 0 && lastWord[lastWord.length - 1] !== word[0]) || arr.includes(word)) {
+            console.log(i)
+            const person = (i + 1) % n === 0 ? n : (i + 1) % n;
+            const turn = Math.ceil((i + 1) / n);
+            return [person, turn];
+        }
+        
+        arr.push(word);
+    }
+    
+    if(arr.length === words.length) return [0, 0];
+}
