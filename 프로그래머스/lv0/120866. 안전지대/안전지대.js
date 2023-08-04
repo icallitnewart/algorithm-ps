bbcca18c5956a2dac2cae3b07cc8dfd30658691e
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-08-04 12:07:58 +0900

    [level 0] Title: 안전지대, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,25 +1,21 @@
 function solution(board) {
     const bombs = [];
-    board.forEach((row, rowIdx)=> {
-        row.forEach((col, colIdx)=> {
-            if(col===1) bombs.push([rowIdx, colIdx]); 
+    board.forEach((row, x)=> {
+        row.forEach((col, y)=> {
+            if(col===1) {
+                if(board[x-1]?.[y] === 0) board[x-1][y] = 2;
+                if(board[x+1]?.[y] === 0) board[x+1][y] = 2;
+
+                if(board[x-1]?.[y-1] === 0) board[x-1][y-1] = 2;
+                if(board[x]?.[y-1] === 0) board[x][y-1] = 2;
+                if(board[x+1]?.[y-1] === 0) board[x+1][y-1] = 2;
+
+                if(board[x-1]?.[y+1] === 0) board[x-1][y+1] = 2;
+                if(board[x]?.[y+1] === 0) board[x][y+1] = 2;
+                if(board[x+1]?.[y+1] === 0) board[x+1][y+1] = 2;
+            }
         });
     });
-    
-    for(let bomb of bombs) {
-        const [x, y] = bomb;
-        
-        if(board[x-1]?.[y] === 0) board[x-1][y] = 2;
-        if(board[x+1]?.[y] === 0) board[x+1][y] = 2;
-        
-        if(board[x-1]?.[y-1] === 0) board[x-1][y-1] = 2;
-        if(board[x]?.[y-1] === 0) board[x][y-1] = 2;
-        if(board[x+1]?.[y-1] === 0) board[x+1][y-1] = 2;
-        
-        if(board[x-1]?.[y+1] === 0) board[x-1][y+1] = 2;
-        if(board[x]?.[y+1] === 0) board[x][y+1] = 2;
-        if(board[x+1]?.[y+1] === 0) board[x+1][y+1] = 2;
-    } 
 
     return board.flat().filter(pos => pos === 0).length;
 }
