6484562a53f7fed46f0543cac9c6aad9efa91932
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-08-08 12:12:27 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 6.79 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -1,12 +1,13 @@
 function solution(s) {
     let [changes, zeros] = [0, 0];
     while(s !== '1') {
-        changes++;
-        zeros += s.split('').filter(n=> n === '0').length;
-        
-        const len = s.replaceAll('0', '').length;
+        const zeroCount = s.split('').filter(n=> n === '0').length;
+        const len = s.length - zeroCount;
         const num = len.toString(2);
+        
         s = num.toString();
+        changes++;
+        zeros += zeroCount;
     }
     
     return [changes, zeros];
