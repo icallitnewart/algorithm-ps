e04d26a6300f7a7798bb35ead5f4490ac29d5e79
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-08-15 23:13:49 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 2.90 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.js
@@ -1,11 +1,4 @@
 function solution(arr) {
-    var answer = [];
-    const smallest = [...arr].sort((a, b)=> parseInt(a) - parseInt(b))[0];
-    
-    if(arr.length === 1) {
-        answer.push(-1);
-    } else {
-        answer = [...arr].filter(n=> n > smallest);
-    }
-    return answer;
+    const nums = arr.filter((_, i)=> i !== arr.indexOf(Math.min(...arr)));
+    return nums.length === 0 ? [-1] : nums;
 }
