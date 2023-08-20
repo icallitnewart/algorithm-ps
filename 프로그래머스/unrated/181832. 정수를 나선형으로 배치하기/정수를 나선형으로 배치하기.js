17719f96b40292a743114189ec15b238d2bac9de
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-08-20 20:18:50 +0900

    [unrated] Title: 정수를 나선형으로 배치하기, Time: 0.41 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
+++ 프로그래머스/unrated/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.js
@@ -1,35 +1,36 @@
 function solution(n) {
-    const arr = Array.from({length: n}, (_, i)=> Array(n).fill(0));
+    const arr = Array.from({length: n}, () => Array(n).fill(0));
+
+    //(좌=>우), (상=>하), (우=>좌), (하=>상) 방향으로 움직일 때 x와 y의 변화
+    const dx = [0, 1, 0, -1]; 
+    const dy = [1, 0, -1, 0];
+
     let num = 1;
-    let cycle = 0;
-    
+    let x = 0, y = 0, dir = 0;
+
     while(num <= n * n) {
-        //left to right
-        for(let i=cycle; i<n-cycle; i++) {
-            arr[cycle][i] = num++;
-        } 
-        
-        //top to bottom
-        for(let i=cycle+1; i<n-cycle; i++) {
-            arr[i][n-cycle-1] = num++;
+        // 현재 칸이 배열 내부에 있고 아직 숫자가 채워지지 않았다면
+        if ((x >= 0 && x < n) && (y >= 0 && y < n) && (arr[x][y] === 0)) {
+            //숫자 채우기
+            arr[x][y] = num++;
+            
+            //방향에 따라 x, y 값 변경
+            x += dx[dir];
+            y += dy[dir];
+            
+        } else { // 다음 칸이 배열 범위 밖이거나 이미 채워진 경우
+            // x, y 값을 다시 배열 범위 안으로 넣어준다
+            x -= dx[dir];
+            y -= dy[dir];
+            
+            // 방향 변경
+            dir = (dir + 1) % 4;
+            
+            // 방향에 따라 x, y 값 변경
+            x += dx[dir];
+            y += dy[dir];
         }
-    
-        //right to left
-        if(cycle < n-cycle-1) {
-            for(let i=n-cycle-2; i>=cycle; i--) {
-                arr[n-cycle-1][i] = num++;
-            }
-        }
-    
-        //bottom to top
-        if(cycle < n-cycle-1) {
-            for(let i=n-cycle-2; i>cycle; i--) {
-                arr[i][cycle] = num++;
-            }
-        }
-        
-        cycle++;
     }
-    
+
     return arr;
 }
