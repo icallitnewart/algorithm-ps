65d6dc03ac12981e2f4399b6d8e695ccf860752f
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-08-24 11:20:15 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,10 +1,3 @@
 function solution(n) {
-    let answer = '';
-    const letters = [ "수", "박" ];
-    
-    for(let i=1; i<=n; i++) {
-        if(i % 2 === 0) answer += letters[1];
-        else answer += letters[0];
-    }
-    return answer;
+    return Array.from({length: n}, (_, i)=> i % 2 > 0 ? '박' : '수').join('');
 }
