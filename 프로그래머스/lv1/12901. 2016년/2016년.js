bfd5f390187df4f9eced759ee29a673d887378c3
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-08-29 12:03:01 +0900

    [level 1] Title: 2016년, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12901. 2016년/2016년.js
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -1,5 +1,7 @@
 function solution(a, b) {
-    const days = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-    const idx = new Date(`2016-${a}-${b}`).getDay();
-    return days[idx];
+    const days = ['FRI', 'SAT', 'SUN', 'MON', 'TUE', 'WED', 'THU'];
+    const dates = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    const idx = dates.slice(0, a).reduce((acc, cur, i)=> i === a - 1 ? acc + b : acc + cur, 0);
+    
+    return days[(idx - 1) % 7];
 }
