5bb6d32f4f5f0793e82d5e657e05d2fbb9c08822
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-08-30 18:22:12 +0900

    [level 2] Title: 땅따먹기, Time: 64.03 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.js
@@ -1,8 +1,8 @@
 function solution(land) {
     //각 행에서 얻을 수 있는 최대 점수 계산하여 갱신
-    const scores = land.slice(1).reduce((acc, cur) => {
+    const scores = land.reduce((acc, cur) => {
         //현재 칸의 점수 + 이전 행에서 얻을 수 있는 최대 점수 (인덱스 겹치지 않게)
         return cur.map((v, i) => v + Math.max(...acc.filter((_, idx) => idx !== i)));
-    }, land[0]);
+    }, [0, 0, 0, 0]);
     return Math.max(...scores);
 }
