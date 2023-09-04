e178e7b154f9e1d0016a9c8ad9d5575e4d763ee4
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-09-04 11:45:45 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    let answer = 0;
+    
+    while(n > 0) {
+        if(n % 2 > 0) {
+            n--;
+            answer++;
+        } else {
+            n /= 2;
+        }
+    }
+    
+    return answer;
+}
