d5c8fc3a77285a0012ebf31126f8f6f207a50bdf
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-09-12 11:23:03 +0900

    [level 1] Title: 소수 만들기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12977. 소수 만들기/소수 만들기.js
@@ -0,0 +1,21 @@
+function solution(nums) {
+    let answer = 0;
+    const len = nums.length;
+    const isPrime = (n)=> {
+        for(let i=2; i<=n/2; i++) {
+            if(n % i === 0) return false;
+        }
+        return true;
+    };
+    
+    for(let i=0; i<len; i++) {
+        for(let j=i+1; j<len; j++) {
+            for(let k=j+1; k<len; k++) {
+                const sum = nums[i] + nums[j] + nums[k];
+                if(isPrime(sum)) answer++;
+            }
+        }
+    }
+    
+    return answer;
+}
