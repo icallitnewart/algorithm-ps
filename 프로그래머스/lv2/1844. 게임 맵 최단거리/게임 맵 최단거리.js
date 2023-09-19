9e2ddffd02f277d21da07b3dedf8e758ee03aede
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-09-19 11:14:06 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.58 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.js
@@ -0,0 +1,34 @@
+function solution(maps) {
+    //x, y 좌표, 거리 기록
+    const queue = [ [0, 0, 1] ];
+    //상, 하, 좌, 우 이동시 거리
+    const dx = [0, 0, -1, 1], dy = [-1, 1, 0, 0];
+    //방문한 곳 체크
+    const visited = Array.from({length: maps.length}, () => new Array(maps[0].length).fill(false));
+
+    //시작점 방문 체크
+    visited[0][0] = true;
+    
+    while(queue.length) {
+        const [x, y, distance] = queue.shift();
+        
+        //마지막 지점 도달시 거리 return
+        if(x === maps[0].length - 1 && y === maps.length -1) return distance;
+        
+        for(let i=0; i<4; i++) {
+            const nextX = x + dx[i], nextY = y + dy[i];
+            
+            //다음 칸이 맵을 벗어나지 않는지 확인
+            if(nextX >= 0 && nextX < maps[0].length &&
+              nextY >= 0 && nextY < maps.length) {
+                //방문 여부, 벽이 없는지 확인
+                if(!visited[nextY][nextX] && maps[nextY][nextX]) {
+                    queue.push([nextX, nextY, distance + 1]);
+                    maps[nextY][nextX] = 0;
+                }
+            }
+        }
+    }
+    
+    return -1;
+}
