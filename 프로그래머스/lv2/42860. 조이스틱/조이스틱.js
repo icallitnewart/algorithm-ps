49d88df7d0f187b2ef6e125fc2365c32c82a50c7
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-09-22 10:19:32 +0900

    [level 2] Title: 조이스틱, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.js
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.js
@@ -2,7 +2,7 @@ function solution(name) {
     let answer = 0;
     const alphabets = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const len = name.length;
-    let move = len - 1; //최대 이동 회수
+    let move = len - 1; //앞으로 쭉 가는 경우 (최대 이동 회수)
 
     for(let i=0; i<len; i++) {    
         //상하 조작
@@ -15,9 +15,12 @@ function solution(name) {
         //A가 아닌 다음 문자열 인덱스 찾기
         while(next < len && name[next] === 'A') next++;
         
-        //쭉 가는 경우
-        const straight = i + len - next;
-        move = Math.min(move, straight + Math.min(i, len - next));
+        //앞으로 갔다 뒤로 가는 경우
+        const forwardAndBackward = i + i + (len - next);
+        //뒤로 갔다 앞으로 가는 경우 (예: BAAAABBBB)
+        const backwardAndForward = i + (len - next) + (len - next);
+        
+        move = Math.min(move, forwardAndBackward, backwardAndForward);
     }
     
     return answer + move;
