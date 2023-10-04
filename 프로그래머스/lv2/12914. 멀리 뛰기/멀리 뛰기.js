a58ac4b7cbcc0aedf3d82c5b711c227f765e2bc3
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-10-04 12:13:14 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,3 +1,20 @@
+//피보나치 수열
+function solution(n) {
+    if(n <= 2) return n;
+    
+    let a = 1, b = 2;
+    
+    for(let i=3; i<=n; i++) {
+        const nextJump = (a + b) % 1234567;
+        a = b;
+        b = nextJump;
+    }
+    
+    return b;
+}
+
+/*
+//동적 프로그래밍 (PASS)
 function solution(n) {
     const dp = new Array(n+1).fill(0);
     
@@ -11,19 +28,30 @@ function solution(n) {
     return dp[n];
 }
 
-/*
-//피보나치 수열
+//조합 (FAIL)
+function factorial(n) {
+    if(n <= 1) return 1;
+    let result = 1;
+    for(let i=2; i<=n; i++) result *= i;
+    return result;
+}
+
+// n! / (r! * (n - r)!)
+function getCombinations(n, r) {
+    return factorial(n) / (factorial(r) * factorial(n -r));
+}
+
 function solution(n) {
-    if(n <= 2) return n;
+    let answer = 0;
+    let numOfTwo = 0;
     
-    let a = 1, b = 2;
-    
-    for(let i=3; i<=n; i++) {
-        let temp = b;
-        b = (a + b) % 1234567;
-        a = temp;
+    while(numOfTwo <= n / 2) {
+        const numOfJumps = numOfTwo + (n - numOfTwo * 2);
+        answer += getCombinations(numOfJumps, numOfTwo);
+        numOfTwo++;
     }
     
-    return b;
+    
+    return answer % 1234567;
 }
 */
