8f31d798ec2e84535cd6b1ddb16318062cab2751
icallitnewart <65598117+icallitnewart@users.noreply.github.com>
2023-10-04 11:49:16 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/lv2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,4 +1,19 @@
 function solution(n) {
+    const dp = new Array(n+1).fill(0);
+    
+    dp[1] = 1;
+    if(n > 1) dp[2] = 2;
+    
+    for(let i=3; i<=n; i++) {
+        dp[i] = (dp[i-1] + dp[i-2]) % 1234567;
+    }
+    
+    return dp[n];
+}
+
+/*
+//피보나치 수열
+function solution(n) {
     if(n <= 2) return n;
     
     let a = 1, b = 2;
@@ -11,3 +26,4 @@ function solution(n) {
     
     return b;
 }
+*/
